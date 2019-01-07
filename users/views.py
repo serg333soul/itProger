47ff10256e694553b155168b09b562473837bca4
Serg_home <serg333soul@gmail.com>
2019-01-07 11:49:18 +0200

    end_reg_lesson

--- users/views.py
+++ users/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserOurRegistration
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserOurRegistration(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Пользователь {username} был успешно зарегистрирован!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserOurRegistration()
     return render(request, 'users/registration.html', {'form': form, 'title': 'Регистрация пользователя'})
